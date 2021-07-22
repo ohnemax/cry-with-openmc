eda989dbbb775c2ce391495616bb36c37ec473f2
Moritz <moritz@nuclearfreesoftware.org>
2021-07-22 19:16:53 +0200

    Allow for user defined cutoff source energy

--- source_cosmic_ray_neutrons.cpp
+++ source_cosmic_ray_neutrons.cpp
@@ -18,11 +18,24 @@ public:
   double xoffset;
   double yoffset;
   double zoffset;
+  double cutoffenergy;
+  bool discard;
   std::vector<CRYParticle*> *vect; // vector of generated particles
 
   CustomSource(std::string parameters) {
 
     int pos = parameters.find(' ');
+    cutoffenergy = std::stod(parameters.substr(0, pos));
+    parameters.erase(0, pos + 1);
+    pos = parameters.find(' ');
+    if(parameters.substr(0, pos) == "discard") {
+      discard = true;
+    }
+    else {
+      discard = false; // If not discard, particle with e > cutoffenergy get e = cutoffenergy
+    }
+    parameters.erase(0, pos + 1);
+    pos = parameters.find(' ');
     xoffset = std::stod(parameters.substr(0, pos));
     parameters.erase(0, pos + 1);
     pos = parameters.find(' ');
@@ -32,6 +45,7 @@ public:
     zoffset = std::stod(parameters.substr(0, pos));
     parameters.erase(0, pos + 1);
 
+    // std::cout << cutoffenergy << ", " << discard << std::endl;
     // std::cout << xoffset << ", " << yoffset << ", " << zoffset << std::endl;
     // std::cout << parameters;
     
@@ -78,15 +92,33 @@ public:
     // }
 
     CRYParticle* p = (*vect)[0];
-
+    double e = p->ke() * 1e6;
+    if(discard) {
+      while(e > cutoffenergy) {
+        // std::cout << "Discarding particle, looking for particle below cutoff energy" << std::endl;
+        delete p;
+        vect->erase(vect->begin());
+        if(vect->size() == 0) {
+          gen->genEvent(vect);
+        }
+        p = (*vect)[0];
+        e = p->ke() * 1e6;
+      }
+    }
+    else {
+      if(e > cutoffenergy) {
+        e = cutoffenergy - 0.001; // be 1meV below cutoff energy
+      }
+    }
+    particle.E = e;
     particle.r.x = p->x() * 100 + xoffset;
     particle.r.y = p->y() * 100 + yoffset;
     particle.r.z = p->z() * 100 + zoffset;
-    // particle.E = (*vect)[j]->ke() * 1e6;
     particle.u = {p->u(), p->v(), p->w()};
-    particle.E = p->ke() * 1e6;
+
     delete p;
-    
+    // std::cout << particle.E << std::endl;
+    // std::cout << particle.r << std::endl;
     vect->erase(vect->begin());
     return particle;
     
@@ -101,7 +133,6 @@ double forwarder(void* context) {
 
 extern "C" std::unique_ptr<CustomSource> openmc_create_source(std::string parameters)
 {
-  std::cout << "can do something in openmc_create_source" << std::endl;
   auto a = std::unique_ptr<CustomSource> (new CustomSource(parameters));
   a->setup->setRandomFunctionWithContext(forwarder, a.get());
   
